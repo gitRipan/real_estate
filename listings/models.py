8777d60ce06dfd1a82db0826fc181eb23e84d16b
gitRipan <uddeepan13@gmail.com>
2021-05-29 19:58:48 +0530

    Added Home & About page dynamic content

--- listings/models.py
+++ listings/models.py
@@ -1,6 +1,7 @@
-from django.db import models
 from datetime import datetime
+from django.db import models
 from realtors.models import Realtor
+
 
 class Listing(models.Model):
     realtor = models.ForeignKey(Realtor, on_delete=models.DO_NOTHING)
@@ -16,13 +17,13 @@ class Listing(models.Model):
     garage = models.IntegerField(default=0)
     sqft = models.IntegerField()
     lot_size = models.DecimalField(max_digits=5, decimal_places=1)
-    photo_main = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
-    photo_1 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
-    photo_2 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
-    photo_3 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
-    photo_4 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
-    photo_5 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
-    photo_6 = models.ImageField(upload_to = 'photos/%Y/%m/%d/', blank=True)
+    photo_main = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_1 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_2 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_3 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_4 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_5 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
+    photo_6 = models.ImageField(upload_to='photos/%Y/%m/%d/', blank=True)
     is_published = models.BooleanField(default=True)
     list_date = models.DateTimeField(default=datetime.now, blank=True)
 
