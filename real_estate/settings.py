3a6a4cb0e5c59b190c41f60453b1b843f7819d2b
gitRipan <uddeepan13@gmail.com>
2021-06-04 21:05:41 +0530

    Added Dashboard functionality

--- real_estate/settings.py
+++ real_estate/settings.py
@@ -140,6 +140,13 @@ MESSAGE_TAGS = {
     messages.ERROR: 'danger',
 }
 
+# Email config
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER='uddeepan13@gmail.com'
+EMAIL_HOST_PASSWORD='madrid13'
+EMAIL_USE_TLS=True
+
 # Default primary key field type
 # https://docs.djangoproject.com/en/3.2/ref/settings/#default-auto-field
 
